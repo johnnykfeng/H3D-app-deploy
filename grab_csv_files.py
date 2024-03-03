e70c788fadefc746ce00645b2b872956a304cbc5
John Feng <johnfengphd@gmail.com>
2024-03-03 15:09:33 -0800

    grabbed csv files from height test

--- grab_csv_files.py
+++ grab_csv_files.py
@@ -27,11 +27,12 @@ def save_csv_files(directory: str, destination_dir: str):
         shutil.copy2(source_file, destination_file)
 
 if __name__ == "__main__":
-    source_directory = r"Y:\M2766\R&D\Jira-RD-748" # Yuxin's data
+    # source_directory = r"Y:\M2766\R&D\Jira-RD-748" # Yuxin's data
+    source_directory = r"Z:\R&D\Personal\JohnF\H3D_MAPPER-DATA\Varying Source Height Test Data - No Mask"
     csv_files = find_csv_files(source_directory)
-    # ic(csv_files)
+    ic(csv_files)
     # Destination folder in the local repository
-    destination_dir = r"C:\Users\10552\OneDrive - Redlen Technologies\Code\H3D-app-deploy\data\leaky_pixel_data" 
+    destination_dir = r"C:\Users\10552\OneDrive - Redlen Technologies\Code\H3D-app-deploy\data\height_test_data" 
     save_csv_files(source_directory, destination_dir)
     
     
